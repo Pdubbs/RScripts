426a251881245cdf8e2a7ddf7dbb64bae8ee748c
Pdubbs <mr.wyatt@gmail.com>
2014-05-29 16:38:24 -0500

    Sample splitting and null lists

--- RScripts.r
+++ RScripts.r
@@ -1,3 +1,22 @@
+list <- structure(NA,class="result")
+"[<-.result" <- function(x,...,value) {
+   args <- as.list(match.call())
+   args <- args[-c(1:2,length(args))]
+   length(value) <- length(args)
+   for(i in seq(along=args)) {
+     a <- args[[i]]
+     if(!missing(a)) eval.parent(substitute(a <- v,list(a=a,v=value[[i]])))
+   }
+   x
+} #taken from https://stat.ethz.ch/pipermail/r-help/2004-June/053343.html
+
+sample_split<-function(data,percentile=.2){
+  sample_list<-sample(1:nrow(data),nrow(data)*percentile,replace=FALSE)
+  train<-data[-sample_list,]
+  test<-data[sample_list,]
+  return(list(train,test))
+}
+
 na_filler<-function(data,ints_as_factors=FALSE,character_fill=""){
   for(col in colnames(data)){
     if(class(data[,col])=="numeric"|(class(data[,col])=="integer"&ints_as_factors==FALSE)){
@@ -10,3 +29,5 @@ na_filler<-function(data,ints_as_factors=FALSE,character_fill=""){
   }
   return(data)
 }
+
+
